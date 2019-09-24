7176e634e1dac055c3c0c8e22cd8c42d420575f6
Rudy <rudy@itprovent.com>
2019-09-24 13:14:50 +0700

    facebook & instagram table report

--- src/components/FacebookReport.js
+++ src/components/FacebookReport.js
@@ -2,33 +2,26 @@ import React, { Component } from 'react'
 import { Table, Image } from 'semantic-ui-react'
 import moment from 'moment'
 
-export default class InstagramReport extends Component {
+export default class FacebookReport extends Component {
     state = {
         data: [],
         viewTable: []
     }
 
     loadInstagramReport = () => {
-        window.FB.api(`/${this.props.id}/media`, {fields: 'caption,media_type,thumbnail_url,media_url,username,timestamp'}, function(response) {
+        window.FB.api('/me/feed', {fields: 'caption,created_time,description,full_picture'}, function(response) {
             console.log(response);
             let data = response.data;
             let viewTable = [];
-            let imageView;
             for (const i in data) {
                 if (data.hasOwnProperty(i)) {
                     const item = data[i];
-                    if(item.media_type === "VIDEO") {
-                        imageView = <Table.Cell>{item.thumbnail_url ? <Image src={item.thumbnail_url} size='small' rounded></Image> : null}</Table.Cell>;
-                    } else {
-                        imageView = <Table.Cell>{item.media_url ? <Image src={item.media_url} size='small' rounded></Image> : null}</Table.Cell>;
-                    }
                     viewTable.push(
                         <Table.Row key={i}>
-                            <Table.Cell>{moment(item.timestamp).locale('id').format("DD/MM/YYYY, HH:mm:ss")}</Table.Cell>
-                            <Table.Cell>{item.username ? item.username : null}</Table.Cell>
-                            <Table.Cell>{item.media_type ? item.media_type : null}</Table.Cell>
-                            {imageView}
+                            <Table.Cell>{moment(item.created_time).locale('id').format("DD/MM/YYYY, HH:mm:ss")}</Table.Cell>
+                            <Table.Cell>{item.full_picture ? <Image src={item.full_picture} size='small' rounded></Image> : null}</Table.Cell>
                             <Table.Cell>{item.caption ? item.caption : null}</Table.Cell>
+                            <Table.Cell>{item.description ? item.description : null}</Table.Cell>
                         </Table.Row>);
                 }
             }
@@ -49,11 +42,10 @@ export default class InstagramReport extends Component {
             <Table celled padded>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell>username</Table.HeaderCell>
                         <Table.HeaderCell singleLine>Created Time</Table.HeaderCell>
-                        <Table.HeaderCell>Type</Table.HeaderCell>
                         <Table.HeaderCell>Picture</Table.HeaderCell>
                         <Table.HeaderCell>Caption</Table.HeaderCell>
+                        <Table.HeaderCell>Description</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>{this.state.viewTable}</Table.Body>
--- src/components/Report.js
+++ src/components/Report.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
-import { Header, Grid, Image, Menu, Container } from 'semantic-ui-react';
+import { Header, Grid, Image, Menu, Container, Divider } from 'semantic-ui-react';
 import InstagramReport from './InstagramReport';
+import FacebookReport from './FacebookReport';
 
 export default class Report extends Component {
     state = {
@@ -17,13 +18,18 @@ export default class Report extends Component {
     }
 
     responseFacebook = (response) =>  {
-        this.setState({
-            isLoggedIn: true,
-            userId: response.userId,
-            name: response.name,
-            email: response.email,
-            picture: response.picture.data.url
-        });
+        let result = response;
+        window.FB.api('/me/accounts', {fields: 'instagram_business_account'}, function(response) {
+            console.log(response);
+            this.setState({
+                isLoggedIn: true,
+                igAccount: response.data[0].instagram_business_account.id,
+                userId: result.userId,
+                name: result.name,
+                email: result.email,
+                picture: result.picture.data.url
+            });
+        }.bind(this));
     }
 
     handleItemClick = (e, { name }) => {
@@ -64,9 +70,14 @@ export default class Report extends Component {
                             Sign-out
                         </Menu.Item>
                     </Menu>
-                    <Container fluid>
-                        <Header as='h1'>Instagram Table Report</Header>
-                        <InstagramReport />
+                    <Container>
+                        <Container fluid>
+                            <Header as='h1'>Instagram Table Report</Header>
+                            <InstagramReport id={this.state.igAccount} />
+                            <Divider />
+                            <Header as='h1'>Facebook Table Report</Header>
+                            <FacebookReport />
+                        </Container>
                     </Container>
                 </div>
             );
